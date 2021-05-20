77dc004eb58ca928f8bb8aef189927d5b790ca1a
github username <github email address>
2021-05-19 22:37:44 -0400

    update controller actions

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ItemsController < ApplicationController
 
     def index
         items = Item.all
-        render json: items
+        render json: ItemSerializer.new(items)
     end
 
     def create 
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :title, :description, :image_url, :url, :category_id, :category
 end
